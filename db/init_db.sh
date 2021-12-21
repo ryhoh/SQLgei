af05d5cfb3c93383489e6320a12316a35071a920
ryhoh <axis38akasira@gmail.com>
2021-12-21 17:30:53 +0900

    Update init_db.sh
    ポート番号変更

--- db/init_db.sh
+++ db/init_db.sh
@@ -2,9 +2,9 @@
 
 rm -rf ./psql
 pg_ctl initdb -D ./psql
-pg_ctl start -D ./psql
-psql -d postgres -f db_definitions.sql
+pg_ctl start -D ./psql -o "-p 54321"
+psql -d postgres -p 54321 -f db_definitions.sql
 
-psql -d sandbox -U maintainer -f table_definitions.sql
+psql -d sandbox -U maintainer -p 54321 -f table_definitions.sql
 
 pg_ctl stop -D ./psql
